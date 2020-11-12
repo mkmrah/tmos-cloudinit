42ce1a9b197e195911443897f3b140d63ed2752c
mkmrah <39188713+mkmrah@users.noreply.github.com>
2020-11-12 19:30:59 +0530

    Update compute.tf

--- demo/ibmcloud_vpc_gen2/schematics/tmos_multi_nic/compute.tf
+++ demo/ibmcloud_vpc_gen2/schematics/tmos_multi_nic/compute.tf
@@ -67,11 +67,11 @@ locals {
 locals {
   # custom image takes priority over public image
   # image_id = data.ibm_is_image.tmos_custom_image.id == null ? lookup(local.public_image_map[var.tmos_image_name], var.region) : data.ibm_is_image.tmos_custom_image.id
-   image_id = "r006-1ca34358-b1f0-44b1-bf9a-a8bd9837a672" 
+   #image_id = "r006-1ca34358-b1f0-44b1-bf9a-a8bd9837a672" 
   # stage
   #image_id = "r134-8741e499-52b6-46c0-91e0-6fa7ec72c78b"
   # public image takes priority over custom image
-  #image_id = lookup(lookup(local.public_image_map, var.tmos_image_name, {}), var.region, data.ibm_is_image.tmos_custom_image.id)
+  image_id = lookup(lookup(local.public_image_map, var.tmos_image_name, {}), var.region, data.ibm_is_image.tmos_custom_image.id)
   template_file = lookup(local.license_map, var.license_type, local.license_map["none"])
   # user admin_password if supplied, else set a random password
   admin_password = var.tmos_admin_password == "" ? random_password.password.result : var.tmos_admin_password
